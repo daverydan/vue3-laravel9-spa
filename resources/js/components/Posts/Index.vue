a6429e4f4e6129692950faeacea98cffab3b8420
daverydan <danny.w.avery@gmail.com>
2022-03-26 15:46:05 -0400

    Column Filters & Global Search

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -1,36 +1,69 @@
 <template>
-    <div class="mb-4">
-        <select
-            v-model="search_category"
-            class="block mt-1 w-full sm:w-1/4 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-        >
-            <option value="" selected>Filter by category</option>
-            <option
-                v-for="category in categories"
-                :value="category.id"
-                :key="category.id"
-            >
-                {{ category.name }}
-            </option>
-        </select>
-    </div>
-    <div class="overflow-hidden overflow-x-auto bg-white border sm:rounded-lg">
+    <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
         <div class="min-w-full align-middle">
-            <table class="min-w-full divide-y divide-gray-200">
+            <div class="mb-4 grid lg:grid-cols-4 gap-4">
+                <input
+                    v-model="search_global"
+                    type="text"
+                    placeholder="Search..."
+                    class="inline-block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+            </div>
+            <table class="min-w-full divide-y divide-gray-200 border">
                 <thead>
                     <tr>
                         <th class="px-6 py-3 bg-gray-50 text-left">
-                            <!-- <span
-                                class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                                >ID</span
-                            > -->
-                            <!-- justify-between -->
+                            <input
+                                v-model="search_id"
+                                type="text"
+                                class="inline-block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                placeholder="Filter by ID"
+                            />
+                        </th>
+                        <th class="px-6 py-3 bg-gray-50 text-left">
+                            <input
+                                v-model="search_title"
+                                type="text"
+                                class="inline-block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                placeholder="Filter by Title"
+                            />
+                        </th>
+                        <th class="px-6 py-3 bg-gray-50 text-left">
+                            <select
+                                v-model="search_category"
+                                class="inline-block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            >
+                                <option value="" selected>
+                                    -- all categories --
+                                </option>
+                                <option
+                                    v-for="category in categories"
+                                    :value="category.id"
+                                >
+                                    {{ category.name }}
+                                </option>
+                            </select>
+                        </th>
+                        <th class="px-6 py-3 bg-gray-50 text-left">
+                            <input
+                                v-model="search_content"
+                                type="text"
+                                class="inline-block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                placeholder="Filter by Content"
+                            />
+                        </th>
+                        <th class="px-6 py-3 bg-gray-50 text-left"></th>
+                        <th class="px-6 py-3 bg-gray-50 text-left"></th>
+                    </tr>
+                    <tr>
+                        <th class="px-6 py-3 bg-gray-50 text-left">
+                            <!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">ID</span>-->
                             <div
                                 class="flex flex-row items-center cursor-pointer"
                                 @click="updateOrdering('id')"
                             >
                                 <div
-                                    class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                    class="text-xs mr-2 leading-4 font-medium text-gray-500 uppercase tracking-wider"
                                     :class="{
                                         'font-bold text-blue-600':
                                             orderColumn === 'id',
@@ -70,17 +103,13 @@
                             </div>
                         </th>
                         <th class="px-6 py-3 bg-gray-50 text-left">
-                            <!-- <span
-                                class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                                >Title</span
-                            > -->
-                            <!-- justify-between -->
+                            <!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</span>-->
                             <div
                                 class="flex flex-row items-center cursor-pointer"
                                 @click="updateOrdering('title')"
                             >
                                 <div
-                                    class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                    class="text-xs mr-2 leading-4 font-medium text-gray-500 uppercase tracking-wider"
                                     :class="{
                                         'font-bold text-blue-600':
                                             orderColumn === 'title',
@@ -93,13 +122,12 @@
                                         :class="{
                                             'text-blue-600':
                                                 orderDirection === 'asc' &&
-                                                orderColumn === 'title',
+                                                orderColumn === 'id',
                                             hidden:
                                                 orderDirection !== '' &&
                                                 orderDirection !== 'asc' &&
-                                                orderColumn === 'title',
-                                            'opacity-25':
-                                                orderColumn !== 'title',
+                                                orderColumn === 'id',
+                                            'opacity-25': orderColumn !== 'id',
                                         }"
                                         >&uarr;</span
                                     >
@@ -107,13 +135,13 @@
                                         :class="{
                                             'text-blue-600':
                                                 orderDirection === 'desc' &&
-                                                orderColumn === 'title',
+                                                orderColumn === 'id',
                                             hidden:
                                                 orderDirection !== '' &&
                                                 orderDirection !== 'desc' &&
-                                                orderColumn === 'title',
+                                                orderColumn === 'id',
                                             'hidden opacity-25':
-                                                orderColumn !== 'title',
+                                                orderColumn !== 'id',
                                         }"
                                         >&darr;</span
                                     >
@@ -123,27 +151,26 @@
                         <th class="px-6 py-3 bg-gray-50 text-left">
                             <span
                                 class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                                >Content</span
+                                >Category</span
                             >
                         </th>
                         <th class="px-6 py-3 bg-gray-50 text-left">
                             <span
                                 class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                                >Category</span
+                                >Content</span
                             >
                         </th>
-                        <th class="px-6 py-3 bg-gray-50 text-left">
-                            <!-- <span
-                                class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                                >Created at</span
-                            > -->
-                            <!-- justify-between -->
+                        <th
+                            class="px-6 py-3 bg-gray-50 text-center"
+                            style="width: 152px"
+                        >
+                            <!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Created at</span>-->
                             <div
                                 class="flex flex-row items-center cursor-pointer"
                                 @click="updateOrdering('created_at')"
                             >
                                 <div
-                                    class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                    class="text-xs mr-2 leading-4 font-medium text-gray-500 uppercase tracking-wider"
                                     :class="{
                                         'font-bold text-blue-600':
                                             orderColumn === 'created_at',
@@ -156,13 +183,12 @@
                                         :class="{
                                             'text-blue-600':
                                                 orderDirection === 'asc' &&
-                                                orderColumn === 'created_at',
+                                                orderColumn === 'id',
                                             hidden:
                                                 orderDirection !== '' &&
                                                 orderDirection !== 'asc' &&
-                                                orderColumn === 'created_at',
-                                            'opacity-25':
-                                                orderColumn !== 'created_at',
+                                                orderColumn === 'id',
+                                            'opacity-25': orderColumn !== 'id',
                                         }"
                                         >&uarr;</span
                                     >
@@ -170,13 +196,13 @@
                                         :class="{
                                             'text-blue-600':
                                                 orderDirection === 'desc' &&
-                                                orderColumn === 'created_at',
+                                                orderColumn === 'id',
                                             hidden:
                                                 orderDirection !== '' &&
                                                 orderDirection !== 'desc' &&
-                                                orderColumn === 'created_at',
+                                                orderColumn === 'id',
                                             'hidden opacity-25':
-                                                orderColumn !== 'created_at',
+                                                orderColumn !== 'id',
                                         }"
                                         >&darr;</span
                                     >
@@ -187,9 +213,7 @@
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200 divide-solid">
-                    <!-- <tr v-for="post in posts"> -->
-                    <!-- +data for laravel vue pagination -->
-                    <tr v-for="post in posts.data" :key="post.id">
+                    <tr v-for="post in posts.data">
                         <td
                             class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
                         >
@@ -203,12 +227,12 @@
                         <td
                             class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
                         >
-                            {{ post.content }}
+                            {{ post.category }}
                         </td>
                         <td
                             class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
                         >
-                            {{ post.category }}
+                            {{ post.content }}
                         </td>
                         <td
                             class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
@@ -223,16 +247,15 @@
                                     name: 'posts.edit',
                                     params: { id: post.id },
                                 }"
-                                class="bg-gray-100 px-2 py-1 text-xs rounded-sm hover:bg-gray-200 transition duration-200 ease-in-out uppercase tracking-wide mr-1"
+                                class="px-2 py-1 bg-gray-100 hover:bg-gray-200 rounded-sm transition duration-200 ease-in-out"
                                 >Edit</router-link
                             >
                             <a
-                                @click.prevent="deletePost(post.id)"
                                 href="#"
-                                class="bg-red-100 px-2 py-1 text-xs rounded-sm hover:bg-red-200 transition duration-200 ease-in-out uppercase tracking-wide"
+                                @click.prevent="deletePost(post.id)"
+                                class="px-2 py-1 bg-red-100 hover:bg-red-200 rounded-sm transition duration-200 ease-in-out ml-2"
+                                >Delete</a
                             >
-                                Delete
-                            </a>
                         </td>
                     </tr>
                 </tbody>
@@ -241,13 +264,7 @@
             <Pagination
                 :data="posts"
                 @pagination-change-page="
-                    (page) =>
-                        getPosts(
-                            page,
-                            search_category,
-                            orderColumn,
-                            orderDirection
-                        )
+                    (page) => getPosts(page, selectedCategory)
                 "
             />
         </div>
@@ -258,28 +275,21 @@
 import { ref, onMounted, watch } from "vue";
 import usePosts from "../../composables/posts";
 import useCategories from "../../composables/categories";
-
 export default {
     setup() {
         const search_category = ref("");
-        // const search_id = ref("");
-        // const search_title = ref("");
-        // const search_content = ref("");
-        // const search_global = ref("");
+        const search_id = ref("");
+        const search_title = ref("");
+        const search_content = ref("");
+        const search_global = ref("");
         const orderColumn = ref("created_at");
         const orderDirection = ref("desc");
         const { posts, getPosts, deletePost } = usePosts();
         const { categories, getCategories } = useCategories();
-
         onMounted(() => {
             getPosts();
             getCategories();
         });
-
-        watch(search_category, (current, previous) => {
-            getPosts(1, current);
-        });
-
         const updateOrdering = (column) => {
             orderColumn.value = column;
             orderDirection.value =
@@ -287,17 +297,74 @@ export default {
             getPosts(
                 1,
                 search_category.value,
+                search_id.value,
+                search_title.value,
+                search_content.value,
+                search_global.value,
                 orderColumn.value,
                 orderDirection.value
             );
         };
-
+        watch(search_category, (current, previous) => {
+            getPosts(
+                1,
+                current,
+                search_id.value,
+                search_title.value,
+                search_content.value,
+                search_global.value
+            );
+        });
+        watch(search_id, (current, previous) => {
+            getPosts(
+                1,
+                search_category.value,
+                current,
+                search_title.value,
+                search_content.value,
+                search_global.value
+            );
+        });
+        watch(search_title, (current, previous) => {
+            getPosts(
+                1,
+                search_category.value,
+                search_id.value,
+                current,
+                search_content.value,
+                search_global.value
+            );
+        });
+        watch(search_content, (current, previous) => {
+            getPosts(
+                1,
+                search_category.value,
+                search_id.value,
+                search_title.value,
+                current,
+                search_global.value
+            );
+        });
+        watch(search_global, (current, previous) => {
+            getPosts(
+                1,
+                search_category.value,
+                search_id.value,
+                search_title.value,
+                search_content.value,
+                current
+            );
+        });
         return {
             posts,
             getPosts,
             deletePost,
             categories,
             search_category,
+            search_id,
+            search_title,
+            search_content,
+            search_global,
             orderColumn,
             orderDirection,
             updateOrdering,
